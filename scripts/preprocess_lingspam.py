0362121a4bef1655a3076a5489bc5848ef36e4ad
tita-ubuntu-vm <i.nimah@tue.nl>
2017-07-07 23:30:18 +0100

    updates

--- scripts/preprocess_lingspam.py
+++ scripts/preprocess_lingspam.py
@@ -32,3 +32,45 @@ if __name__ == '__main__':
 	savePickle(content,'allMails')
 	savePickle(numCont,'allNumMails')
 
+	## For mail subject (short text part of mail)
+	#######################################################
+	# create WE version of subject using gensim word2vec model
+	# put all subjects into one single array list, with separated class label array
+
+	classLabel=[]
+	subjSentences = []
+	for i in subject:
+		nclass = len(subject[i])
+		for _j in range(nclass):
+			classLabel.append(i)
+		subjSentences += subject[i]
+
+	savePickle(subjSentences,'ls_subjSentences')
+	savePickle(classLabel,'ls_classLabel')
+
+	subjNumSentences = []
+	for i in subject:
+		subjNumSentences += numSubj[i]
+
+	savePickle(subjNumSentences,'ls_subjNumSentences')
+
+
+	## For mail contents
+	#######################################################
+
+	
+	mailSentences = []
+	for i in content:
+		mailSentences += content[i]
+
+	savePickle(mailSentences,'ls_mailSentences')
+
+	mailNumSentences = []
+	for i in numCont:
+		mailNumSentences += numCont[i]
+
+	savePickle(mailNumSentences,'ls_mailNumSentences')
+
+
+	
+
